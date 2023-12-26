1d27cd22b3f501aad2e8357e40764ffdaefc5753
chdv7 <chdv.github@telesoft.spb.ru>
2023-12-26 21:43:40 +0300

    Solve warnings

--- PduSMS.cpp
+++ PduSMS.cpp
@@ -21,24 +21,23 @@ struct T_UDH{
 #pragma pack(pop)
 
 
-static int MAX_TEXT_LEN[3]= {160, 140, 70};
-static const int UdhLenInChars[3]= {7, 6, 3};
+static const unsigned char MAX_TEXT_LEN[3]= {160, 140, 70};
+static const unsigned char UdhLenInChars[3]= {7, 6, 3};
 
 static unsigned char gReference((unsigned char)time(NULL));
 
 
-unsigned short lookup7Bit[256] = { // lookup table to check possibility convert 8 bit character to 7 bit
+unsigned char lookup7Bit[256] = { // lookup table to check possibility convert 8 bit character to 7 bit
 // values
 // 0x00-0x7f code
 // 0x80-0xfe esc code flag + code;
 // 0xFF - not possible 
-// mask 0xff00 high byte does not used for encoding, but makes possible to store character @ with GSM code 0
 //	  0     1     2     3     4     5     6     7     8     9     A     B     C     D     E     F
 	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0A, 0xFF, 0x8A, 0x0D, 0xFF, 0xFF,  // 0
 	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,  // 1
 	0x20, 0x21, 0x22, 0x23, 0x02, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,  // 2
 	0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,  // 3
-      0xff00, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,  // 4
+    0x00, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,  // 4
 	0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0xBC, 0xAF, 0xBE, 0x94, 0x11,  // 5
 	0xFF, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,  // 6
 	0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0xA8, 0xC0, 0xA9, 0xBD, 0xFF,  // 7
@@ -50,7 +49,6 @@ unsigned short lookup7Bit[256] = { // lookup table to check possibility convert 
 	0xFF, 0x5D, 0xFF, 0xFF, 0xFF, 0xFF, 0x5C, 0xFF, 0x0B, 0xFF, 0xFF, 0xFF, 0x5E, 0xFF, 0xFF, 0x1E,  // D
 	0x7F, 0xFF, 0xFF, 0xFF, 0x7B, 0x0F, 0x1D, 0xFF, 0x04, 0x05, 0xFF, 0xFF, 0x07, 0xFF, 0xFF, 0xFF,  // E
 	0xFF, 0x7D, 0x08, 0xFF, 0xFF, 0xFF, 0x7C, 0xFF, 0x0C, 0x06, 0xFF, 0xFF, 0x7E, 0xFF, 0xFF, 0xFF,  // F
-
 };
 
 
@@ -255,7 +253,7 @@ string COutPduSms::MakeSmsSubmitHeader(bool isHasUDH, PDU_EncodingScheme encodin
 	if (m_nTTL>0)    nSmsSubmit |= 0x10;  // TP-VPF b10 - Relative format
 	hdr += BinToText(nSmsSubmit); // FirstOctet of SMS Submit
 	hdr += "00";                  // TP-Message-Reference
-	hdr += BinToText(phoneNo.length());	// Address Length (phone len)
+	hdr += BinToText((unsigned char)phoneNo.length());	// Address Length (phone len)
 
 										// Type-Of-Address
 	if (!m_sPone.empty() && m_sPone[0] == '+')
@@ -278,7 +276,7 @@ string COutPduSms::MakeSmsSubmitHeader(bool isHasUDH, PDU_EncodingScheme encodin
 	hdr += BinToText(tp_dcs); // TP-DCS
 
 	if (m_nTTL>0)
-		hdr += BinToText(m_nTTL);			// TTL if need
+		hdr += BinToText((unsigned char)m_nTTL);	// TTL if need
 
 	return hdr;
 }
@@ -292,7 +290,7 @@ static string encode7bitStep8to7 (const wchar_t* text, int len){
 		unsigned char chr = (unsigned char) text[i]&0x7f;
 		bout[i] = chr>>i;
 		if (i>0)
-			bout[i-1] |= chr<< (8-i);
+			bout[i-1] |= (unsigned char) (chr<< (8-i));
 	}
 	if (len==8)
 		len=7;
@@ -352,7 +350,7 @@ TOutPduBlock COutPduSms::ParseText(void)
 		}
 
 		if (!_8bitFound && !_16bitFound && sms7BitText.length()*7/8 >= sText.length())
-			_8bitFound = true; // force 8 bit if too meny esc characters found
+			_8bitFound = true; // force 8 bit if too many esc characters found
 
 		if (_16bitFound)
 			encodingScheme = PDU_16;
@@ -372,6 +370,9 @@ TOutPduBlock COutPduSms::ParseText(void)
 		maxPartSize -= UdhLenInChars[encodingScheme]; // Reserve space for header
 
 	int nParts = (textLen-1)/maxPartSize+1;
+//	ToDo: check for max SMS parts
+//	if (nParts > 255) // ToDo check for max SMS parts
+
 
 	string smsPartHdr = MakeSmsSubmitHeader (hasUDH, encodingScheme); // Header common for all parts of SMS (next byte is TP-USER-DATA-LENGTH
 //	TRACE ("%s\n", smsPartHdr.c_str());
@@ -386,10 +387,10 @@ TOutPduBlock COutPduSms::ParseText(void)
 
 	TOutPduBlock outList;
 
-	for (int iPart=0; iPart<nParts; ++iPart){
+	for (unsigned char iPart=0; iPart<nParts; ++iPart){
 		string smsText = smsPartHdr;
-		int partSZ = min(textLen, maxPartSize);
-		int dataSZ = (hasUDH) ? partSZ + UdhLenInChars[encodingScheme] : partSZ;
+		unsigned char partSZ = (unsigned char) min(textLen, maxPartSize);
+		unsigned char dataSZ = (hasUDH) ? partSZ + UdhLenInChars[encodingScheme] : partSZ;
 		if (encodingScheme == PDU_16)
 			dataSZ<<=1;
 		smsText += BinToText(dataSZ);  // UDL in bytes including UDH for 7 bit encoding UDL is	in septets 
