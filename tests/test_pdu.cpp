c27a3040879bd157c2e82f723d6d91ab06d78d4e
chdv7 <chdv.github@telesoft.spb.ru>
2025-11-16 01:32:18 +0300

    initially add gtest

---
+++ tests/test_pdu.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+// твой заголовок, путь подстрой
+#include "../src/PduSMS.h"
+
+TEST(PduSmsTest, SimpleEncodeDecode)
+{
+    // Пока пусть будет заглушка, лишь бы хоть один тест был
+    SUCCEED();
+}
